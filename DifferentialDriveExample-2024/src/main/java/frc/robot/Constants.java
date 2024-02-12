54487e8b231defd07cf363cc185fa9d0a99024dc
v0ncent <banksvincent5@gmail.com>
2024-02-12 16:17:15 -0600

    2024 version of test robot

---
+++ DifferentialDriveExample-2024/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */ 
+public final class Constants {
+    public static final int FRONTLEFTMOTOR_PORT = 2;
+    public static final int FRONTRIGHTMOTOR_PORT = 9;
+    public static final int BACKRIGHTMOTOR_PORT = 6;
+    public static final int BACKLEFTMOTOR_PORT = 5;
+    public static final class Controllers {
+        public static final int JOYSTICK_PORT = 0;
+        public static final int JOYSTICKY = 1;
+        public static final int JOYSTICKZ = 2;
+    }
+
+    public static final double TORQUE_RESISTANCE_THRESHOLD = 0.05F;
+    public static final double JOYSTICK_DEADZONE_DRIVE = 0.01F;
+    public static final double JOYSTICK_DEADZONE_ROTATE = 0.01F;
+
+    public static final double MOVEMENT_NERF = 1.5F;
+}
---
+++ DifferentialDriveExample-2024/src/main/java/frc/robot/commands/Drive.java
@@ -0,0 +1,45 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants;
+import frc.robot.subsystems.DriveTrain;
+
+public class Drive extends Command {
+    private final CommandXboxController controller;
+    private final DriveTrain driveTrain;
+
+    public Drive(CommandXboxController controller, DriveTrain driveTrain) {
+        this.driveTrain = driveTrain;
+        this.controller = controller;
+        addRequirements(driveTrain);
+    }
+
+    @Override
+    public void initialize() {
+        
+    }
+
+    @Override
+    public void execute() {
+        // need to test this new driving code.
+        double rotate = driveTrain.deadZone(controller.getRawAxis(Constants.Controllers.JOYSTICKY), Constants.JOYSTICK_DEADZONE_ROTATE);
+        double forwardBack =  driveTrain.deadZone(controller.getRawAxis(Constants.Controllers.JOYSTICKZ), Constants.JOYSTICK_DEADZONE_DRIVE);
+
+        driveTrain.arcadeDrive(driveTrain.applyCurve(rotate / Constants.MOVEMENT_NERF), driveTrain.applyCurve(forwardBack));
+
+        // to test out other driving method comment out above and uncomment below
+        // driveTrain.semiConstantCurvatureDrive(forwardBack, rotate);
+    }
+
+    @Override
+    public void end(boolean interuppted) {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+    
+}
